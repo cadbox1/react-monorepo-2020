aa4ea01d8e3dcfe3223d1743c02ddae3fe34d924
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2022-11-26 12:45:51 +0000

    update component theme layout as per new version

--- packages/components/src/theme.tsx
+++ packages/components/src/theme.tsx
@@ -1,15 +1,10 @@
 // @ts-ignore
+import React from 'react';
 import { bootstrap } from "@theme-ui/presets";
 
 export default {
 	...bootstrap,
 	sizes: { ...bootstrap.sizes, container: "800px" },
-	layout: {
-		...bootstrap.layout,
-		container: {
-			px: 2,
-		},
-	},
 	buttons: {
 		base: {
 			cursor: "pointer",
